8bde18cd11c9e501433a4bf7623a4d72728b3615
Alexandru Iorga <78234213+AlexIorga11@users.noreply.github.com>
2021-05-11 16:22:16 +0300

    Update proiect final

--- Proiect SDA/src/problema8/Text.java
+++ Proiect SDA/src/problema8/Text.java
@@ -22,10 +22,9 @@ class Fraza
 
 	public int Vocale()
 	{
-		String vocale = "AEIOUYWaeiouyw";
+		String vocale = "AEIOUaeiou";
 		char[] aux=F.toCharArray();
 		short count_vocale = 0;
-		short count_consoane = 0;
 
 		for(int i = 0; i < aux.length; i++)
 		{
@@ -35,11 +34,28 @@ class Fraza
 			{
 				count_vocale++;
 			}
-			
-			else
-				count_consoane++;
+
 		}
-		return count_vocale & count_consoane;
+		return count_vocale;
+	}
+
+	public int Consoane()
+	{
+		String consoane = "qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM";
+		char[] aux=F.toCharArray();
+		short count_consoane = 0;
+
+		for(int i = 0; i < aux.length; i++)
+		{
+			String a = String.valueOf(aux[i]);
+
+			if(consoane.contains(a))
+			{
+				count_consoane++;
+			}
+
+		}
+		return count_consoane;
 	}
 
 	public boolean Final(String F)
@@ -50,22 +66,45 @@ class Fraza
 		}
 		return false;
 	}
-
-	public void Majuscule()
-	{
-		F = F.toUpperCase();
-	}
-
 }
 
 public class Text {
 	public static void main(String[] args) throws IOException
 	{
-		Fraza[] frz = new Fraza[3000];
-		
+		Fraza[] fraze = new Fraza[3000];
+
 		short i = 0;
 		short j = 0;
-		
-		BufferedReader write = new BufferedReader(new InputStreamReader(new FileInputStream("src\\problema8\\Cantec_in.txt")));
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("./src/problema8/text.txt")));
+
+		String buffer;
+		while((buffer = reader.readLine()) != null)
+		{
+			fraze[i] = new Fraza(buffer);
+			i++;
+		}
+
+		reader.close();
+
+		for(j = 0; j < i; j++)
+		{
+			System.out.printf("Randul %d " + " \n" , j + 1);
+			float M = (float)(fraze[j].Consoane() + fraze[j].Vocale()) / (fraze[j].Cuvinte());
+			float N = 1/M;
+			float p = (float)(fraze[j].Vocale()) / (fraze[j].Consoane());
+			float P = p * N;
+			System.out.println("Cuvinte = " + fraze[j].Cuvinte() + " | " + "Consoane = " + fraze[j].Consoane() + " | " + "Vocale = " + fraze[j].Vocale());
+			/*System.out.printf("N = %.2f " + " ", N);
+			System.out.printf("p = %.2f " + " ", p);*/
+			System.out.printf("M = %.2f " + " ", M);
+			System.out.printf("P = %.4f " + " \n" , P);
+			System.out.println("=-----------------------------------------------=");
+		}
+		System.out.println("{ Legenda }");
+		System.out.println("M - Lungimea medie a cuvintelor");
+		System.out.println("P - Pronuntabilitatea");
+		System.out.println("M = (nr. vocale + nr. consoane) / nr. cuvinte");
+		System.out.println("P = (nr. vocale / nr. consoane) * (1 / M)");
 	}
 }
